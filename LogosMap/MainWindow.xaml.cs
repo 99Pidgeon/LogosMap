e8371b224da7d92e6dbea417fbf7f961df282e4b
99Pidgeon <enironir@gmail.com>
2025-05-08 01:31:57 +0900

    GPU 렌더링 전환

--- LogosMap/MainWindow.xaml.cs
+++ LogosMap/MainWindow.xaml.cs
@@ -14,22 +14,45 @@ namespace LogosMap
         private SKPoint translate = new(0, 0);
         private float scale = 1.0f;
 
-        public Node? moveNode;
+        public Node? selectedNode;
         public static List<Node> nodes = [];
         public static Dictionary<int, Node> nodeIds = new();
         private SKPoint clickPosition;
         private SKPoint startDragPosition;
 
-        public List<Node> selectedNode = [];
+        private bool isEdited;
 
         private bool isPanning = false;
 
         public static MainWindow? Instance { get; private set; }
 
+        SKPaint paint;
+        SKPaint paint2;
+
+        public static string FileName = "새 마인드맵";
+        public static string FileDirectory = "";
+
         public MainWindow()
         {
             InitializeComponent();
             Instance = this;
+            mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
+
+            paint = new SKPaint
+            {
+                Color = SKColors.White,
+                Style = SKPaintStyle.Fill,
+                IsAntialias = true,
+            };
+            font.Typeface = GetTypeface("GMARKETSANSTTFLIGHT.TTF");
+
+            paint2 = new SKPaint
+            {
+                Color = SKColor.Parse("#4FFFFFFF"),
+                StrokeWidth = 0.6f,
+                IsAntialias = true
+            };
+
             AddNewNode(new SKPoint(400, 250));
         }
 
@@ -53,16 +76,54 @@ namespace LogosMap
         {
             if (e.Key == Key.S && (Keyboard.Modifiers & ModifierKeys.Control) != 0)
             {
+                if (FileDirectory == null || FileDirectory == "")
+                {
+                    var saveFileDialog = new SaveFileDialog
+                    {
+                        Title = "저장...",
+                        Filter = "JSON 파일 (*.json)|*.json",
+                        FileName = "새 마인드맵.json"
+                    };
+
+                    if (saveFileDialog.ShowDialog() == true)
+                    {
+                        string filePath = saveFileDialog.FileName;
+                        FileName = saveFileDialog.SafeFileName;
+                        FileDirectory = filePath;
+
+                        isEdited = false;
+                        mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
+
+                        SaveLoad.SaveMindMap(filePath);
+                    }
+                }
+                else
+                {
+                    isEdited = false;
+                    mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
+
+                    SaveLoad.SaveMindMap(FileDirectory);
+                }
+            }
+
+            if (e.Key == Key.S && (Keyboard.Modifiers & ModifierKeys.Control) != 0 && (Keyboard.Modifiers & ModifierKeys.Shift) != 0)
+            {
                 var saveFileDialog = new SaveFileDialog
                 {
-                    Title = "저장...",
-                    Filter = "JSON 파일 (*.json)|*.json|모든 파일 (*.*)|*.*",
-                    FileName = "NewMap.json"
+                    Title = "다른 이름으로 저장...",
+                    Filter = "JSON 파일 (*.json)|*.json",
+                    FileName = FileName
                 };
 
                 if (saveFileDialog.ShowDialog() == true)
                 {
                     string filePath = saveFileDialog.FileName;
+                    FileName = saveFileDialog.SafeFileName;
+                    FileDirectory = filePath;
+
+                    isEdited = false;
+                    mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
+
                     SaveLoad.SaveMindMap(filePath);
                 }
             }
@@ -75,7 +136,7 @@ namespace LogosMap
 
         private SKFont font = new();
 
-        private void SkCanvas_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        private void SkCanvas_PaintSurface(object sender, SKPaintGLSurfaceEventArgs e)
         {
             SKCanvas canvas = e.Surface.Canvas;
 
@@ -84,34 +145,7 @@ namespace LogosMap
             canvas.Save();
             canvas.Translate(translate.X, translate.Y);
             canvas.Scale(scale);
-            var paint = new SKPaint
-            {
-                Color = SKColor.FromHsl(255, 255, 255, 255),
-                Style = SKPaintStyle.Fill,
-                IsAntialias = true,
-            };
-            var selectedPaint = new SKPaint
-            {
-                Color = SKColor.FromHsl(255, 255, 255, 255),
-                StrokeWidth = 1,
-                Style = SKPaintStyle.Fill,
-                IsAntialias = true,
-            };
-            var selectedPaintStroke = new SKPaint
-            {
-                Color = SKColor.Parse("#00FF00"),
-                StrokeWidth = 1,
-                Style = SKPaintStyle.Stroke,
-                IsAntialias = true,
-            };
-            font.Typeface = GetTypeface("GMARKETSANSTTFLIGHT.TTF");
 
-            var paint2 = new SKPaint
-            {
-                Color = SKColors.Gray,
-                StrokeWidth = 1,
-                IsAntialias = true
-            };
             foreach (var node in nodes)
             {
                 foreach (Connection connection in node.connections)
@@ -129,16 +163,8 @@ namespace LogosMap
                 );
 
                 if (!bounds.IntersectsWith(nodeRect)) continue;
-                if(selectedNode.Contains(node))
-                {
-                    canvas.DrawCircle(node.x, node.y, 6f, selectedPaint);
-                    canvas.DrawCircle(node.x, node.y, 6f, selectedPaintStroke);
-                }
-                else
-                {
-                    canvas.DrawCircle(node.x, node.y, 6f, paint);
-                }
 
+                canvas.DrawCircle(node.x, node.y, 6f, paint);
                 canvas.DrawText(node.name, new SKPoint(node.x, node.y + 20), SKTextAlign.Center, font, paint);
             }
 
@@ -185,26 +211,17 @@ namespace LogosMap
             }
             else if (clickedNode != null)
             {
-                if ((Keyboard.Modifiers & ModifierKeys.Shift) != 0)
-                {
-                    selectedNode.Add(clickedNode);
-                }
-                else
-                {
-                    selectedNode.Clear();
-                    selectedNode.Add(clickedNode);
-                }
+                selectedNode = clickedNode;
 
-                skCanvas.InvalidateVisual();
-                moveNode = clickedNode;
+                isEdited = true;
+                mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
+
                 EndEditing();
             }
             else
             {
                 startDragPosition = new SKPoint((float)e.GetPosition(this).X, (float)e.GetPosition(this).Y - 18);
                 skCanvas.CaptureMouse();
-                selectedNode.Clear();
-                skCanvas.InvalidateVisual();
                 EndEditing();
                 isPanning = true;
             }
@@ -237,13 +254,11 @@ namespace LogosMap
                 }
                 else
                 {
-                    Node newNode = AddNewNode(position);
-                    selectedNode.Clear();
-                    selectedNode.Add(newNode);
-                    moveNode = newNode;
-                    ConnectNodes(clickedNode, newNode);
+                    selectedNode = AddNewNode(position);
+                    ConnectNodes(clickedNode, selectedNode);
                 }
-                    
+                isEdited = true;
+                mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
             }
             else
             {
@@ -272,6 +287,8 @@ namespace LogosMap
             {
                 DeleteNode(node);
                 skCanvas.InvalidateVisual();
+                isEdited = true;
+                mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
             };
 
             menu.Items.Add(addNode);
@@ -287,9 +304,9 @@ namespace LogosMap
                 isPanning = false;
 
             }
-            if (moveNode != null)
+            if (selectedNode != null)
             {
-                moveNode = null;
+                selectedNode = null;
             }
         }
 
@@ -299,24 +316,64 @@ namespace LogosMap
             nodeIds.Clear();
             lastId = 0;
 
+            FileDirectory = "";
+            FileName = "새 마인드맵";
+            isEdited = false;
+
             AddNewNode(new SKPoint(400, 250));
 
             translate = new SKPoint(0, 0);
         }
 
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        private void SaveAsButton_Click(object sender, RoutedEventArgs e)
         {
             var saveFileDialog = new SaveFileDialog
             {
-                Title = "저장...",
-                Filter = "JSON 파일 (*.json)|*.json|모든 파일 (*.*)|*.*",
-                FileName = "NewMap.json"
+                Title = "다른 이름으로 저장...",
+                Filter = "JSON 파일 (*.json)|*.json",
+                FileName = FileName
             };
 
             if (saveFileDialog.ShowDialog() == true)
             {
                 string filePath = saveFileDialog.FileName;
+                FileName = saveFileDialog.SafeFileName;
+                FileDirectory = filePath;
+                isEdited = false;
+                mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
                 SaveLoad.SaveMindMap(filePath);
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FileDirectory == null || FileDirectory == "")
+            {
+                var saveFileDialog = new SaveFileDialog
+                {
+                    Title = "저장...",
+                    Filter = "JSON 파일 (*.json)|*.json",
+                    FileName = "새 마인드맵.json"
+                };
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    FileName = saveFileDialog.SafeFileName;
+                    FileDirectory = filePath;
+
+                    isEdited = false;
+                    mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
+
+                    SaveLoad.SaveMindMap(filePath);
+                }
+            }
+            else
+            {
+                isEdited = false;
+                mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
+
+                SaveLoad.SaveMindMap(FileDirectory);
             }
         }
 
@@ -325,12 +382,16 @@ namespace LogosMap
             var openFileDialog = new OpenFileDialog
             {
                 Title = "불러오기...",
-                Filter = "JSON 파일 (*.json)|*.json|모든 파일 (*.*)|*.*"
+                Filter = "JSON 파일 (*.json)|*.json"
             };
 
             if (openFileDialog.ShowDialog() == true)
             {
                 string filePath = openFileDialog.FileName;
+                FileName = openFileDialog.SafeFileName;
+                FileDirectory = filePath;
+                isEdited = false;
+                mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
                 SaveLoad.LoadMindMap(filePath);
             }
 
@@ -339,11 +400,11 @@ namespace LogosMap
 
         private void Canvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
-            if (moveNode != null)
+            if (selectedNode != null)
             {
-                editingNode = moveNode;
-                EditorBox.Text = moveNode.name;
-                moveNode.name = "";
+                editingNode = selectedNode;
+                EditorBox.Text = selectedNode.name;
+                selectedNode.name = "";
 
                 skCanvas.InvalidateVisual();
 
@@ -359,7 +420,7 @@ namespace LogosMap
                 EditorBox.Width = MathF.Max(50, font.MeasureText(EditorBox.Text, paint) + 10);
                 EditorBox.Height = 20;
 
-                SKPoint BoxPos = new(moveNode.x, moveNode.y);
+                SKPoint BoxPos = new(selectedNode.x, selectedNode.y);
 
                 Point ScreenPos = GetScreenPosition(BoxPos);
 
@@ -367,7 +428,7 @@ namespace LogosMap
                 EditorBox.Focus();
                 EditorBox.SelectAll();
 
-                moveNode = null;
+                selectedNode = null;
             }
         }
 
@@ -375,11 +436,11 @@ namespace LogosMap
         {
             var position = GetPosition(e.GetPosition(this));
 
-            if (moveNode != null)
+            if (selectedNode != null)
             {
                 var offset = position - clickPosition;
 
-                SetNodePosition(moveNode, moveNode.x + offset.X, moveNode.y + offset.Y);
+                SetNodePosition(selectedNode, selectedNode.x + offset.X, selectedNode.y + offset.Y);
             }
             else
             {
@@ -489,12 +550,11 @@ namespace LogosMap
         private void DeleteNode(Node node)
         {
             editingNode = null;
-            moveNode = null;
+            selectedNode = null;
 
             foreach(var connection in node.startConnections)
             {
                 connection.endNode.connections.Remove(connection);
-                connection.startNode.startConnections.Remove(connection);
             }
             nodeIds.Remove(node.Id);
             nodes.Remove(node);
@@ -531,6 +591,10 @@ namespace LogosMap
                 editingNode.name = EditorBox.Text != "" ? EditorBox.Text : "노드";
                 EditorBox.Visibility = Visibility.Collapsed;
                 skCanvas.InvalidateVisual();
+
+                isEdited = true;
+                mainWindow.Title = !isEdited ? "로고스맵 - " + FileName : "로고스맵 - " + FileName + "*";
+
                 editingNode = null;
             }
         }
